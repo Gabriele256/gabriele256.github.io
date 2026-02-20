9e4ffc4e4f52fb57e2900a58388910e05d24d7a0
Gabriele <113119077+Gabriele256@users.noreply.github.com>
2026-02-20 20:56:02 +0100

    Update footer.tsx

--- app/_components/sections/footer.tsx
+++ app/_components/sections/footer.tsx
@@ -13,12 +13,17 @@ export default function Footer() {
 
                 <div className="text-gray-500 text-xs hidden md:block">
                     <p className="inline">Built with </p>
-                    <Link href={"https://nextjs.org/"} className="text-white">
+                    <Link
+                        href={"https://nextjs.org/"}
+                        target={"_blank"}
+                        className="text-white"
+                    >
                         Next.js
                     </Link>
                     <p className="inline"> & </p>
                     <Link
                         href={"https://tailwindcss.com/"}
+                        target={"_blank"}
                         className="text-cyan-400"
                     >
                         Tailwind
@@ -28,12 +33,16 @@ export default function Footer() {
                 <div className="flex gap-6">
                     <Link
                         href={"https://github.com/Gabriele256/"}
+                        target={"_blank"}
                         className="text-gray-400 hover:text-sky-400 transition-colors"
                     >
                         <FaGithub size={20} />
                     </Link>
                     <Link
-                        href={"https://www.linkedin.com/in/gabriele-rossi-7320533b2/"}
+                        href={
+                            "https://www.linkedin.com/in/gabriele-rossi-7320533b2/"
+                        }
+                        target={"_blank"}
                         className="text-gray-400 hover:text-sky-400 transition-colors"
                     >
                         <FaLinkedin size={20} />
